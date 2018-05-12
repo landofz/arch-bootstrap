a38a6239d4af2e6f2c57ef4a3cef291cf8dafcd4
Zoran Melis <zoran@landofz.net>
2018-05-12 23:39:26 +0200

    Improve locale settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -111,7 +111,11 @@ arch-chroot /mnt hwclock --systohc
 sed -i -e 's/^#en_US.UTF-8/en_US.UTF-8/' /mnt/etc/locale.gen
 sed -i -e 's/^#hr_HR.UTF-8/hr_HR.UTF-8/' /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
-echo "LANG=hr_HR.UTF-8" > /mnt/etc/locale.conf
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
+echo "LC_COLLATE=hr_HR.UTF-8" >> /mnt/etc/locale.conf
+echo "LC_MONETARY=hr_HR.UTF-8" >> /mnt/etc/locale.conf
+echo "LC_NUMERIC=hr_HR.UTF-8" >> /mnt/etc/locale.conf
+echo "LC_TIME=hr_HR.UTF-8" >> /mnt/etc/locale.conf
 sed -i -e 's/issue_discards = 0/issue_discards = 1/' /mnt/etc/lvm/lvm.conf
 
 ### Set up bootloader ###
