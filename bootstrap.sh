625d977778bf27d426fdac9358e061ee01b36687
Zoran Melis <zoran@landofz.net>
2018-05-12 22:39:21 +0200

    Fix incorrect path

--- bootstrap.sh
+++ bootstrap.sh
@@ -110,7 +110,7 @@ arch-chroot /mnt hwclock --systohc
 sed -i -e 's/^#en_US.UTF-8/en_US.UTF-8/' /mnt/etc/locale.gen
 sed -i -e 's/^#hr_HR.UTF-8/hr_HR.UTF-8/' /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
-echo "LANG=hr_HR.UTF-8" > /etc/locale.conf
+echo "LANG=hr_HR.UTF-8" > /mnt/etc/locale.conf
 sed -i -e 's/issue_discards = 0/issue_discards = 1/' /mnt/etc/lvm/lvm.conf
 
 ### Set up bootloader ###
